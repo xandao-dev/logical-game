9c74fdeac273df99df9dceb8f0e4a600a357c451
xandao6 <alexandrecalilmf@gmail.com>
2019-12-07 03:06:41 -0300

    added a random expression

--- logicalGame.py
+++ logicalGame.py
@@ -4,37 +4,49 @@
 @author: xandao6
 """
 
+import random
+
 true = 'True'
 false = 'False'
 op_not = 'not'
 op_and = 'and'
 op_or = 'or'
 
-booleans = [true, false]
-logical_operators = [op_and, op_or]
-
-expression = [true, op_or, false, op_and, true, op_and, true, op_or, true]
-
 
 def main():
+    random.seed()
     print("\nSolve this:")
+    expression = generate_expression(1)
     print_list_formated(expression)
     user_answer = input("What's the answer (true/false)? ")
     print('\n')
 
+    print_list_formated(expression)
     while len(expression) > 1:
         for i in range(len(expression)):
             if i < (len(expression) - 2):
-                finded_or = verify_operator_OR(i)
+                finded_or = verify_operator_OR(i, expression)
                 if finded_or == True:
                     print_list_formated(expression)
             if i < (len(expression) - 2):
-                finded_and = verify_operator_AND(i)
+                finded_and = verify_operator_AND(i, expression)
                 if finded_and == True:
                     print_list_formated(expression)
-    print_game_result(user_answer)
-                
-    
+    print_game_result(user_answer, expression)
+
+
+def generate_expression(level):
+    #expression = [true, op_or, false, op_and, true, op_and, true, op_or, true]
+    expression = []
+    booleans = [true, false]
+    logical_operators = [op_and, op_or]
+
+    for i in range(7):  # Even Number
+        if i % 2 == 0:
+            expression.append(random.choice(booleans))
+        else:
+            expression.append(random.choice(logical_operators))
+    return expression
 
 
 def remove_3_in_sequence_from_list(my_list, first_index):
@@ -51,7 +63,7 @@ def print_list_formated(my_list):
     print(' '.join(my_list))
 
 
-def verify_operator_OR(position):
+def verify_operator_OR(position, expression):
     # OPERADOR OR
     if expression[position] == true and expression[position+1] == op_or and expression[position+2] == true:
         remove_3_in_sequence_from_list(expression, position)
@@ -69,11 +81,11 @@ def verify_operator_OR(position):
         remove_3_in_sequence_from_list(expression, position)
         insert_1_in_list(expression, position, true)
         return True
-    else: 
+    else:
         return False
 
 
-def verify_operator_AND(position):
+def verify_operator_AND(position, expression):
     # OPERADOR AND
     if expression[position] == true and expression[position+1] == op_and and expression[position+2] == true:
         remove_3_in_sequence_from_list(expression, position)
@@ -91,11 +103,11 @@ def verify_operator_AND(position):
         remove_3_in_sequence_from_list(expression, position)
         insert_1_in_list(expression, position, false)
         return True
-    else :
+    else:
         return False
 
 
-def print_game_result(user_answer):
+def print_game_result(user_answer, expression):
     if (expression[0] == true and user_answer == 'true') or \
             (expression[0] == false and user_answer == 'false'):
         print("\nYou're right! Congratulations!!! \U0001F44F")
